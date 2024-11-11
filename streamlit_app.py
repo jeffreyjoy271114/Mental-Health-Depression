768070565a526cd4966cf673a3d76d8f0f7312c2
Jeffrey Joy <56599489+jeffreyjoy271114@users.noreply.github.com>
2024-11-11 16:41:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Title for the app
 st.title("Mental Health and Depression Analysis")
